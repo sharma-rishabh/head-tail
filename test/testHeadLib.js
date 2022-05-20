83e02ab6f3736d6b4599cd9ecea662d345fe4158
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-05-20 12:51:00 +0530

    Changed test for extractData to reflect the expected data type.

--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -3,7 +3,7 @@ const { extractData, splitBy, joinBy, head } = require('../src/headLib.js');
 
 describe('extractData', () => {
   it('should give array back if array has only 10 elements.', () => {
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
     return assert.deepStrictEqual(extractData(array), array);
   });
   it('should give the whole array back if length is less than 10.', () => {
@@ -12,8 +12,9 @@ describe('extractData', () => {
   });
   it('should give first 10 elements back if array length is more than 10.',
     () => {
-      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      const expectedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      const array =
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
+      const expectedArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
       return assert.deepStrictEqual(extractData(array), expectedArray);
     });
 });
